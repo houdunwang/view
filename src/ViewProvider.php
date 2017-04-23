bdbb27ea08cfb7be3f8032ec283d425f90194d1e
houdunwang <2300071698@qq.com>
2017-04-23 19:59:58 +0800

    no message

--- src/ViewProvider.php
+++ src/ViewProvider.php
@@ -7,23 +7,27 @@
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
+
 namespace houdunwang\view;
 
-use houdunwang\config\Config;
 use houdunwang\framework\build\Provider;
 
-class ViewProvider extends Provider {
+class ViewProvider extends Provider
+{
+    //延迟加载
+    public $defer = true;
 
-	//延迟加载
-	public $defer = true;
+    public function boot()
+    {
+    }
 
-	public function boot() {
-	}
-
-	public function register() {
-		Config::set( 'view.compile_open', Config::get( 'app.debug' ) );
-		$this->app->single( 'View', function () {
-			return View::single();
-		} );
-	}
+    public function register()
+    {
+        $this->app->single(
+            'View',
+            function () {
+                return View::single();
+            }
+        );
+    }
 }
